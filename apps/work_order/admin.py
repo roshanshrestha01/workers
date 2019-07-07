ee944fc8afea6adb8f53beb23d06619cf0373324
Roshan Shrestha <roshanshrestha01@gmail.com>
2019-07-07 19:52:28 +0545

    Proper displaying Worker and Work Order in admin page

--- apps/work_order/admin.py
+++ apps/work_order/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Worker, WorkOrder
+
+
+class WorkerAdmin(admin.ModelAdmin):
+    list_display = ['name', 'company', 'email', ]
+    search_fields = ['name', 'company', 'email', ]
+
+
+admin.site.register(Worker, WorkerAdmin)
+
+
+class WorkOrderAdmin(admin.ModelAdmin):
+    list_display = ['title', 'deadline', ]
+    search_fields = ['title', ]
+
+
+admin.site.register(WorkOrder, WorkOrderAdmin)
